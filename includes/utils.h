5c7870da9c99a7173cbd595f5be92a06048f536f
Dexter88xD <soulaymanehamdan2@gmail.com>
2025-05-07 17:04:44 +0100

    Refactor parser logic: improve ft_find_end, reorganise functions, add token setter
    - Improved the condition logic in ;
    - Moved and reorganised several functions for better modularity.
    - Added a new function to set token types (e.g. cmd, arg, operator).

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: sohamdan <sohamdan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 11:16:45 by sohamdan          #+#    #+#             */
-/*   Updated: 2025/05/05 11:14:28 by sohamdan         ###   ########.fr       */
+/*   Updated: 2025/05/07 15:42:03 by sohamdan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include "minishell.h"
 
 int		ft_isspace(int c);
+int		is_operator(char *str);
 
 t_token	*ft_new_node(void *content);
 t_token	*ft_last_node(t_token *lst);
