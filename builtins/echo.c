c0dea6e4dce8f8991a305bad7e05959f50a33eb1
Dexter88xD <soulaymanehamdan2@gmail.com>
2025-05-31 17:01:18 +0100

    feat(ft_echo): implemented -n option parsing for echo
    - Added `ft_skip_echo_option(t_token **argument)` to detect and skip `-n` option
    - Supports multiple 'n's (e.g. `-nnnn`) as valid newline suppression

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,24 +6,41 @@
 /*   By: sohamdan <sohamdan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 10:42:37 by sohamdan          #+#    #+#             */
-/*   Updated: 2025/05/30 00:35:09 by sohamdan         ###   ########.fr       */
+/*   Updated: 2025/05/31 16:59:11 by sohamdan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
-#include "parser.h"
 #include "minishell.h"
+#include "parser.h"
+
+int	ft_skip_echo_option(t_token **argument)
+{
+	int	no_newline;
+	int	i;
+
+	no_newline = 0;
+	i = 0;
+	if ((*argument)->value[i] == '-')
+	{
+		i++;
+		while ((*argument)->value[i] && (*argument)->value[i] == 'n')
+			i++;
+		if (!(*argument)->value[i] && ((*argument)->value[i - 1]
+				&& (*argument)->value[i - 1] == 'n'))
+		{
+			no_newline = 1;
+			(*argument) = (*argument)->next;
+		}
+	}
+	return (no_newline);
+}
 
 int	ft_echo(t_token *argument)
 {
-	int		no_newline;
+	int	no_newline;
 
-	no_newline = 0;
-	if (!ft_strcmp(argument->value, "-n"))
-	{
-		no_newline = 1;
-		argument = argument->next;
-	}
+	no_newline = ft_skip_echo_option(&argument);
 	while (argument && argument->type != OPERATOR)
 	{
 		printf("%s", argument->value);
--- includes/builtin.h
+++ includes/builtin.h
@@ -6,7 +6,7 @@
 /*   By: sohamdan <sohamdan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 11:15:55 by sohamdan          #+#    #+#             */
-/*   Updated: 2025/05/30 16:26:37 by sohamdan         ###   ########.fr       */
+/*   Updated: 2025/05/31 17:00:57 by sohamdan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 # include "minishell.h"
 
 int	ft_echo(t_token *input);
+int	ft_skip_echo_option(t_token **argument);
+
 int	ft_env(t_token *temp);
 
 #endif
