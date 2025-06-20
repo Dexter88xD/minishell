756a407af959b6f387f52ba7b71c058e8b458ee1
evilman030127 <khalidakbour011@gmail.com>
2025-06-20 23:25:32 +0100

    feat(exec): add command path splitting and basic execution
    - Added support for splitting the PATH into an array
    - Split input command into arguments
    - Linked command with its corresponding path
    - Checked if the command exists and is executable
    - Implemented basic execution
    
    Note: This is an early stage of the execution logic and not fully completed yet.

--- executor/excution.c
+++ executor/excution.c
@@ -6,7 +6,7 @@
 /*   By: kakbour <kakbour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 22:02:30 by sohamdan          #+#    #+#             */
-/*   Updated: 2025/06/20 22:00:40 by kakbour          ###   ########.fr       */
+/*   Updated: 2025/06/20 23:15:58 by kakbour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,16 +44,16 @@ int	ft_is_builtin(t_token *token)
 
 int		len_to_allocate(t_token *input)
 {
-	int i;
+	int len;
 
-	i = 0;
+	len = 0;
 	while(input)
 	{
 		if(input->type == CMD || input->type == ARG)
-			i++;	
+			len++;	
 		input = input->next;
 	}
-	return (i);
+	return (len);
 }	
 
 char	**ft_edit_exec_arr(t_token *input)
@@ -83,12 +83,79 @@ char	**ft_edit_exec_arr(t_token *input)
 	return (exec_arr);
 }
 
+void	cmd_not_found()
+{
+	write(2, "command not found\n", 18);
+	exit(127);
+}
+
+char	**split_env(char **env)
+{
+	int		i;
+	char	**path_splited;
+
+	i = 0;
+	while (env[i] && ft_strncmp("PATH=", env[i], 5) != 0)
+		i++;
+	if (env[i] == NULL)
+		cmd_not_found();
+	path_splited = ft_split(env[i] + 5, ':');
+	return (path_splited);
+}
+
+int	check_absolute(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '/')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	check_cmd(char *PATH)
+{
+	if (access(PATH, F_OK) == 0)
+		return (1);
+	return (0);
+}
+
+char	*the_correct_path(char **path_splited, char **cmd)
+{
+	char	*tmp;
+	char	*to_execute;
+	int		i;
+
+	i = 0;
+	if (path_splited == NULL)
+		return (NULL);
+	if (check_absolute(cmd[0]))
+		return (ft_strdup(cmd[0]));
+	while (path_splited[i])
+	{
+		tmp = ft_strjoin(path_splited[i], "/");
+		to_execute = ft_strjoin(tmp, cmd[0]);
+		if (check_cmd(to_execute))
+			break ;
+		free(tmp);
+		free(to_execute);
+		i++;
+	}
+	if (path_splited[i] == NULL)
+		cmd_not_found();
+	return (free(tmp), to_execute);
+}
+
 void	ft_execution_rest(t_token *input)
 {
 	char **exec_arr = ft_edit_exec_arr(input);
-	int i = 0;
-	while (exec_arr[i])
-		printf("-->%s\n", exec_arr[i++]);
+	char **splited_path = split_env(input->env);
+	char *to_execute = the_correct_path(splited_path, exec_arr);
+	execve(to_execute, exec_arr, NULL);
 }
 
 int	ft_excution(t_token *input)
